05ccf04d50b217bb6becd1836b9ade850b5f444c
Mark Allison <mark@stylingandroid.com>
2017-10-13 08:32:23 +0100

    Simplifies logic

--- app/src/main/java/com/stylingandroid/midipad/data/MutableListLiveData.kt
+++ app/src/main/java/com/stylingandroid/midipad/data/MutableListLiveData.kt
@@ -2,41 +2,46 @@ package com.stylingandroid.midipad.data
 
 import android.arch.lifecycle.LiveData
 
+@Suppress("TooManyFunctions")
 internal open class MutableListLiveData<T>(private val list: MutableList<T> = mutableListOf()) :
         MutableList<T> by list,
         LiveData<List<T>>() {
 
     override fun add(element: T): Boolean =
-            list.add(element).updateIfChanged()
+            element.actionAndUpdate { list.add(it) }
 
     override fun add(index: Int, element: T) =
             list.add(index, element).also { updateValue() }
 
     override fun addAll(elements: Collection<T>): Boolean =
-            list.addAll(elements).updateIfChanged()
+            elements.actionAndUpdate { list.addAll(elements) }
 
     override fun addAll(index: Int, elements: Collection<T>): Boolean =
-            list.addAll(index, elements).updateIfChanged()
+            elements.actionAndUpdate { list.addAll(index, it) }
 
     override fun remove(element: T): Boolean =
-            list.remove(element).updateIfChanged()
+            element.actionAndUpdate { list.remove(it) }
 
     override fun removeAt(index: Int): T =
             list.removeAt(index).also { updateValue() }
 
     override fun removeAll(elements: Collection<T>): Boolean =
-            list.removeAll(elements).updateIfChanged()
+            elements.actionAndUpdate { list.removeAll(it) }
 
     override fun retainAll(elements: Collection<T>): Boolean =
-            list.retainAll(elements).updateIfChanged()
+            elements.actionAndUpdate { list.retainAll(it) }
 
     override fun clear() =
-        list.clear().also { updateValue() }
+            list.clear().also { updateValue() }
 
-    private fun Boolean.updateIfChanged(): Boolean {
-        if (this) {
-            updateValue()
-        }
+    override fun set(index: Int, element: T): T =
+            list.set(index, element).also{ updateValue() }
+
+    private fun <T> T.actionAndUpdate(action: (item: T) -> Boolean): Boolean =
+            action(this).applyIfTrue { updateValue() }
+
+    private fun Boolean.applyIfTrue(action: () -> Unit): Boolean {
+        takeIf { it }?.run { action() }
         return this
     }
 
